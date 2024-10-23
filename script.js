8e2ab1efc0d29a239d401ab593ceffa8478ba312
leelasrinivasgowda <seenusrinivasgowda@gmail.com>
2024-10-23 17:12:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,16 +29,7 @@ var treeData = {
                 {
                     name: "03.01.01 Account Management",
                     children: [
-                        { 
-                        name: "Servers" ,
-                        children:[
-                        {
-                            name:'Web Server',
-                        },
-                        {name : 'File Server'},
-                        {name: 'Application Server'}
-                        }
-                        ]
+                        { name: "Servers" },
                         { name: "Workstations" },
                         { name: "Network Devices" },
                         { name: "Cloud Infrastructure" },
