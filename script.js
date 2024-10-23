472aecef9641326d0d28b6849cd02a69799cad37
leelasrinivasgowda <seenusrinivasgowda@gmail.com>
2024-10-23 15:18:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Set up the margins, width, and height for the tree layout
 var margin = { top: 20, right: 120, bottom: 20, left: 120 },
-    width = 1000 - margin.right - margin.left,
-    height = 1600 - margin.top - margin.bottom;
+    width = 960 - margin.right - margin.left,
+    height = 800 - margin.top - margin.bottom;
 
 var i = 0,
     duration = 750,
@@ -345,7 +345,7 @@ function update(source) {
     var nodes = tree.nodes(root).reverse(),
         links = tree.links(nodes);
 
-    nodes.forEach(function (d) { d.y = d.depth * 180; });
+    nodes.forEach(function (d) { d.y = d.depth * 260; });
 
     var node = svg.selectAll("g.node")
         .data(nodes, function (d) { return d.id || (d.id = ++i); });
